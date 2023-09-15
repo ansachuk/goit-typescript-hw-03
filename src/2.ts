5a8ce541381f3f05d160f67e1b8949444fa99fa9
Andrii Sachuk <ansachuk@icloud.com>
2023-09-15 15:04:43 +0300

    finish first and second task

--- src/2.ts
+++ src/2.ts
@@ -15,25 +15,18 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+	constructor(public name: string, private department: string, protected salary: number) {}
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
-
-  getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
-  }
+	getEmployeeDetails() {
+		return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+	}
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+	constructor(name: string, departments: string, salary: number) {
+		super(name, departments, salary);
+		this.salary += 10000;
+	}
 }
-
 
 export {};
