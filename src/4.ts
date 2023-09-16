1cdd702dd758d9a8936f795220fe6ab60b3f0e18
Andrii Sachuk <ansachuk@icloud.com>
2023-09-16 14:13:49 +0300

    task 3 done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,42 @@
+interface IKey {
+	getSignature(): number;
+}
+
+class Key implements IKey {
+	private signature: number;
+
+	constructor() {
+		this.signature = Math.random();
+	}
+
+	getSignature = (): number => this.signature;
+}
+
+interface IPerson {
+	getKey(): number;
+}
+
+class Person implements IPerson {
+	private key: number;
+
+	constructor(key: IKey) {
+		this.key = key.getSignature();
+	}
+
+	getKey = (): number => this.key;
+}
+
+abstract class House {}
+
+class MyHouse implements House {}
+
 const key = new Key();
 
-const house = new MyHouse(key);
 const person = new Person(key);
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
